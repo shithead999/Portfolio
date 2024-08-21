d62f0fe1d6e582d0f0e4ecb98bbc779453e9c3fe
Sakshi Jain <111310463+shithead999@users.noreply.github.com>
2024-08-21 23:17:16 +0530

    Update Resume

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import json
 # --- PATH SETTINGS---
 current_dir=Path(__file__).parent if "__file__" in locals() else Path.cwd
 css_file= current_dir/"styles"/"main.css"
-resume_file=current_dir/"assets"/"Resume.pdf"
+resume_file=current_dir/"assets"/"Resume (3).pdf"
 profile_pic=current_dir/"assets"/"profile-pic.png"
 
 
